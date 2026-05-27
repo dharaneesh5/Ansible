eeef46c7fb35e8e4ce53d6bbefe4e9903c49328f
dharaneesh5 <dharaneesh0349@gmail.com>
2026-05-27 18:13:51 +0530

    Add files via upload

---
+++ wordpress-24-04.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+mkdir -p /usr/local/src/wordpress-24-04/opt/cloudstack/
+mkdir -p /usr/local/src/wordpress-24-04/
+
+wget -O /usr/local/src/wordpress-24-04/opt/cloudstack/mysql-standalone-cleanup.sh \
+  https://raw.githubusercontent.com/dharaneesh5/Ansible/main/mysql-standalone-cleanup.sh
+
+wget -O /usr/local/src/wordpress-24-04/wordpress-24-04.yaml \
+  https://raw.githubusercontent.com/dharaneesh5/Ansible/main/wordpress-24-04.yaml
